fa11970bee3a210144a2d0487e2809a8b8943f28
crowd42 <crowd42@mailoo.org>
2017-11-17 15:37:08 +0000

    rewrite of the code

--- email_extracter.py
+++ email_extracter.py
@@ -2,27 +2,34 @@
 # coding:utf-8
 
 import re
+import argparse
 import sys
 
-if len(sys.argv) < 3:
-    print('Usage: python email_extracter.py [input_file] [output_file]')
+parser = argparse.ArgumentParser(description="Extract file")
+parser.add_argument('-i', '--input_file')
+parser.add_argument('-o', '--output_file')
+args = parser.parse_args()
+if not args.input_file and not args.output_file:
+    parser.print_help()
     sys.exit()
 
 regex = re.compile(r'[\w\.-]+@[\w\.-]+')
 
 # The input file
-filename = sys.argv[1]
+input_file = args.input_file
 
 # The output file.
-output_file = sys.argv[2]
+output_file = args.output_file
 
-with open(filename, 'r') as emailfile:
-    email_list = emailfile.read().lower()
+try:
+    with open(input_file, 'r') as emailfile:
+        email_list = emailfile.read().lower()
+        # Here re.findall() returns a list of all the found email strings
+        emails = re.findall(regex, email_list)
+        # Write the emails into the file, one per line
+        with open(output_file, 'w') as output_file:
+            for email in emails:
+                output_file.write(email + '\n')
 
-# Here re.findall() returns a list of all the found email strings
-emails = re.findall(regex, email_list)
-
-# Write the emails into the file, one per line
-with open(output_file, 'w') as output_file:
-    for email in emails:
-        output_file.write(email + '\n')
+except:
+    print('this file doesn\'t exist')
